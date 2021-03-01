250558dee63acd814ea1269f04f2e152ce05cd3b
Christopher-DeLaTorre <delatorrechris18@gmail.com>
2021-03-01 10:08:11 -0800

    Game12

---
+++ ReactMSVN/msvn/src/Game/GamePgs/Game12.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import '../../App.css';
+import { Link } from 'react-router-dom'; 
+//need to import screen Game13 (the ending screen for link on buttons)
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Game12() {
+	return (
+		<div>
+			<section class="d-flex justify-content-center align-items-center m-5">
+				<div class="column customsection">
+					<a href="#" /*name='user_concentration' value='FEW'*/>
+						<div class="optionsbox buttontext" style="left: 550; top: 400">
+							<header>Visualization of Data, Artificial Intelligence, Organization.</header>
+						</div>
+					</a>
+					<a href="#">
+						<div class="optionsbox buttontext" style="left: 550; top: 485">
+							<header>Background functions, Connecting databases, Fine-tuning.</header>
+						</div>
+					</a>
+					<a href="#">
+						<div class="optionsbox buttontext" style="left: 550; top: 570">
+							<header>Developing user experience/interface, Design, Artistic, Creative.</header>
+						</div>
+					</a>
+					<a href="#">
+						<div class="optionsbox buttontext" style="left: 550; top: 570">
+							<header>Mobile games, Smartphones, Accessibility.</header>
+						</div>
+					</a>
+
+					<div class="backgroundtext align-items-center col-md">
+						<h3 class="name">Dan</h3>
+						<div class="line mb-5"></div>
+						<div class="container d-flex justify-content-center px-5">
+							<p class="mb-5 dialogue">
+							First things first, out of these activities and working concepts, 
+							which do you like the most or interests you the most?
+							</p>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	);
+}
+
+export default Game12;
