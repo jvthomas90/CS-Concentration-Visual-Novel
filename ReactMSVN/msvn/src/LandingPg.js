4f4c9d4fbc1ea178691b3d9d589c33badc75de96
Smerly <69550699+Smerly@users.noreply.github.com>
2021-03-04 20:19:14 -0800

    Add files via upload

--- ReactMSVN/msvn/src/LandingPg.js
+++ ReactMSVN/msvn/src/LandingPg.js
@@ -1,51 +1,48 @@
 import React from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './images/make_school.png';
+import bg from './images/make_school_classroom_background.png';
 
 function LandingPg() {
 	return (
-		<div>
-			<img
-				src="./images/make_school_classroom_background.png"
-				id="bg"
-				alt="MS Background"
-			/>
-			<div class="container">
-				<div class="card text-white" id="hero">
-					<div class="row">
-						<div class="col-6">
-							<div class="card-body">
-								<h5 class="card-title display-1">
-									<strong>Choose Your Concentration</strong>
-								</h5>
-								<h6 class="card-subtitle display-6">
-									Need help choosing a concentration? Try this interactive
-									visual novel!
-								</h6>
-								<div class="row row-cols-1 gy-5 my-1">
-									<div class="col w-100">
-										<Link to={`/start`}>
-											<button class="btn btn-primary w-50">
-												<strong>Start Your Journey!</strong>
-											</button>
-										</Link>
-									</div>
-									<div class="col w-100">
-										<Link>
-											<button class="btn btn-primary px-5 w-50">
-												<strong>About</strong>
-											</button>
-										</Link>
+		<div class="body mt-5">
+			<div class="html ">
+				<img src={bg} id="bg" alt="MS Background" />
+				<div class="container">
+					<div class="card text-white" id="hero">
+						<div class="row">
+							<div class="col-6">
+								<div class="card-body mt-5">
+									<h5 class="card-title display-1" style={{ fontSize: 70 }}>
+										<strong>Choose Your Concentration</strong>
+									</h5>
+									<h6 class="card-subtitle display-6">
+										Need help choosing a concentration? Try this interactive
+										visual novel!
+									</h6>
+									<div class="row row-cols-1 gy-5 my-1">
+										<div class="col w-100">
+											<Link to={`/start`}>
+												<button class="btn btn-primary w-50">
+													<strong>Start Your Journey!</strong>
+												</button>
+											</Link>
+										</div>
+										<div class="col w-100 mt-3">
+											<Link>
+												<button class="btn btn-primary px-5 w-50">
+													<strong>About</strong>
+												</button>
+											</Link>
+										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-						<div class="col-6">
-							<img
-								class="w-100"
-								src="./images/make_school.png"
-								alt="Make School Logo"
-							/>
+							<div class="col-6">
+								<img class="w-100" src={logo} alt="Make School Logo" />
+							</div>
 						</div>
 					</div>
 				</div>
